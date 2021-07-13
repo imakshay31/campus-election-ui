117c4a3aaf4a52977a30d07581e87d88b4986462
Akshay Pandey <codeakshay31@gmail.com>
2021-07-13 10:00:53 +0530

    getPosition added

---
+++ src/components/getPositions.ts
@@ -0,0 +1,18 @@
+export const positions = [
+  {
+    name: 'President',
+    _id: 'dsfadffds32',
+  },
+  {
+    name: 'VC President',
+    _id: 'd11312dffds32',
+  },
+  {
+    name: 'Seceratary',
+    _id: 'de313122ds32',
+  },
+  {
+    name: 'General Secertary',
+    _id: '313edadadfweda',
+  },
+];
